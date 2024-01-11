29df8fb15b38c1be27f3ebc9d579461032ec087e
Francisco Javier Rodríguez Hernández <fj_rh@hotmail.com>
2024-01-11 14:29:59 -0400

    agregada la parte visual de las alarmas

--- src/manteneceParamsForm/MantenanceParamsForm.java
+++ src/manteneceParamsForm/MantenanceParamsForm.java
@@ -13,18 +13,33 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.Box;
 import net.miginfocom.swing.MigLayout;
 import com.toedter.calendar.JDateChooser;
+
+import components.Constants;
+import dataBaseModels.ParamMesuresDescriptions;
+
 import javax.swing.SwingConstants;
 import javax.swing.border.MatteBorder;
 import java.awt.Color;
 import javax.swing.JScrollPane;
 import javax.swing.ScrollPaneConstants;
+import java.awt.Font;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 
 public class MantenanceParamsForm extends FrameModel {
 	private static final long serialVersionUID = 1L;
-	private JTextField textField;
-	private JTextField textField_1;
-
+	private JTextField txtOperator;
+	private JTextField txtInspector;
+	private ArrayList<Parameter> list ;
+	private JDateChooser date1;
+	private JDateChooser date2;
+	private JDateChooser date3;
+	private JDateChooser date4;
+	private JDateChooser date5;
+	private JDateChooser date6;
+	private JDateChooser date7;
 
 	public MantenanceParamsForm()  {
 		super(null, "MEDICION DE PARAMETROS DE FUNCIONAMIENTO", true);
@@ -33,6 +48,7 @@ public class MantenanceParamsForm extends FrameModel {
 		Dimension size = new Dimension(1200, this.getPreferredSize().height);
 		this.setSize(size);
 		JPanel centerPanel = new JPanel();
+		centerPanel.setBackground(Constants.getSurfaceColor());
 		centerPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
 		getContentPane().add(centerPanel, BorderLayout.CENTER);
 		centerPanel.setLayout(new BorderLayout(0, 0));
@@ -45,74 +61,91 @@ public class MantenanceParamsForm extends FrameModel {
 		titlePanel.setLayout(new BoxLayout(titlePanel, BoxLayout.Y_AXIS));
 		
 		JLabel lblNewLabel = new JLabel("EQUIPO: Turbocompresor Solar Centauro 40 ");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel.setForeground(Constants.getTextColor());
 		titlePanel.add(lblNewLabel);
 		
+		Component verticalStrut_1 = Box.createVerticalStrut(10);
+		titlePanel.add(verticalStrut_1);
+		
 		JPanel panel = new JPanel();
+		panel.setForeground(Constants.getTextColor());
+		panel.setBackground(Constants.getSurfaceColor());
 		panel.setAlignmentX(Component.LEFT_ALIGNMENT);
 		titlePanel.add(panel);
 		panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
 		
 		JLabel lblNewLabel_1 = new JLabel("OPERADOR: ");
+		lblNewLabel_1.setForeground(Constants.getTextColor());
 		panel.add(lblNewLabel_1);
 		
-		textField = new JTextField();
-		panel.add(textField);
-		textField.setColumns(10);
+		txtOperator = new JTextField();
+		panel.add(txtOperator);
+		txtOperator.setColumns(10);
+		
+		Component verticalStrut_2 = Box.createVerticalStrut(5);
+		titlePanel.add(verticalStrut_2);
 		
 		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(Constants.getSurfaceColor());
 		panel_1.setAlignmentX(Component.LEFT_ALIGNMENT);
 		titlePanel.add(panel_1);
 		panel_1.setLayout(new BoxLayout(panel_1, BoxLayout.X_AXIS));
 		
 		JLabel lblNewLabel_2 = new JLabel("JEFE DE MANTENIMIETO:");
+		lblNewLabel_2.setForeground(Constants.getTextColor());
 		panel_1.add(lblNewLabel_2);
 		
-		textField_1 = new JTextField();
-		panel_1.add(textField_1);
-		textField_1.setColumns(10);
+		txtInspector = new JTextField();
+		panel_1.add(txtInspector);
+		txtInspector.setColumns(10);
 		
 		Component verticalStrut = Box.createVerticalStrut(10);
 		titlePanel.add(verticalStrut);
 		
 		JPanel descriptionPanel = new JPanel();
+		descriptionPanel.setBorder(new MatteBorder(1, 1, 1, 0, Constants.getTextColor()));
+		descriptionPanel.setBackground(Constants.getSurfaceColor());
 		descriptionPanel.setAlignmentX(Component.LEFT_ALIGNMENT);
 		titlePanel.add(descriptionPanel);
 		descriptionPanel.setLayout(new MigLayout("insets 0, gap 0", "[300px:n,grow,fill][100px:100px:100px][100px:100px:100px][100px:100px:100px][100px:100px:100px][100px:100px:100px][100px:100px:100px][100px:100px:100px][17px:17px:17px]", "[grow]"));
 		
-		JLabel lblNewLabel_3 = new JLabel("DESCRIPTION");
-		lblNewLabel_3.setBorder(new MatteBorder(1, 1, 1, 1, (Color) Color.DARK_GRAY));
+		JLabel lblNewLabel_3 = new JLabel("DESCRIPCIÓN");
+		lblNewLabel_3.setForeground(Constants.getTextColor());
+		lblNewLabel_3.setBorder(null);
 		lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
 		descriptionPanel.add(lblNewLabel_3, "cell 0 0,grow");
 		
-		JDateChooser dateChooser = new JDateChooser();
-		dateChooser.setBorder(new MatteBorder(1, 0, 1, 1, (Color) Color.DARK_GRAY));
-		descriptionPanel.add(dateChooser, "cell 1 0,grow");
+		date1 = new JDateChooser();
+		date1.setBorder(new MatteBorder(0, 1, 0, 0, Constants.getTextColor()));
+		descriptionPanel.add(date1, "cell 1 0,grow");
 		
-		JDateChooser dateChooser_1 = new JDateChooser();
-		dateChooser_1.setBorder(new MatteBorder(1, 0, 1, 1, (Color) new Color(0, 0, 0)));
-		descriptionPanel.add(dateChooser_1, "cell 2 0,grow");
+		date2 = new JDateChooser();
+		date2.setBorder(new MatteBorder(0, 1, 0, 0, Constants.getTextColor()));
+		descriptionPanel.add(date2, "cell 2 0,grow");
 		
-		JDateChooser dateChooser_2 = new JDateChooser();
-		dateChooser_2.setBorder(new MatteBorder(1, 0, 1, 1, (Color) new Color(0, 0, 0)));
-		descriptionPanel.add(dateChooser_2, "cell 3 0,grow");
+		date3 = new JDateChooser();
+		date3.setBorder(new MatteBorder(0, 1, 0, 0, Constants.getTextColor()));
+		descriptionPanel.add(date3, "cell 3 0,grow");
 		
-		JDateChooser dateChooser_3 = new JDateChooser();
-		dateChooser_3.setBorder(new MatteBorder(1, 0, 1, 1, (Color) new Color(0, 0, 0)));
-		descriptionPanel.add(dateChooser_3, "cell 4 0,grow");
+		date4 = new JDateChooser();
+		date4.setBorder(new MatteBorder(0, 1, 0, 0, Constants.getTextColor()));
+		descriptionPanel.add(date4, "cell 4 0,grow");
 		
-		JDateChooser dateChooser_4 = new JDateChooser();
-		dateChooser_4.setBorder(new MatteBorder(1, 0, 1, 1, (Color) new Color(0, 0, 0)));
-		descriptionPanel.add(dateChooser_4, "cell 5 0,grow");
+		date5 = new JDateChooser();
+		date5.setBorder(new MatteBorder(0, 1, 0, 0, Constants.getTextColor()));
+		descriptionPanel.add(date5, "cell 5 0,grow");
 		
-		JDateChooser dateChooser_5 = new JDateChooser();
-		dateChooser_5.setBorder(new MatteBorder(1, 0, 1, 1, (Color) new Color(0, 0, 0)));
-		descriptionPanel.add(dateChooser_5, "cell 6 0,grow");
+		date6 = new JDateChooser();
+		date6.setBorder(new MatteBorder(0, 1, 0, 0, Constants.getTextColor()));
+		descriptionPanel.add(date6, "cell 6 0,grow");
 		
-		JDateChooser dateChooser_6 = new JDateChooser();
-		dateChooser_6.setBorder(new MatteBorder(1, 0, 1, 1, (Color) new Color(0, 0, 0)));
-		descriptionPanel.add(dateChooser_6, "cell 7 0,grow");
+		date7 = new JDateChooser();
+		date7.setBorder(new MatteBorder(0, 1, 0, 1, (Color) new Color(255, 255, 255)));
+		descriptionPanel.add(date7, "cell 7 0,grow");
 		
 		JPanel panel_2 = new JPanel();
+		panel_2.setOpaque(false);
 		descriptionPanel.add(panel_2, "cell 8 0,grow");
 		
 		JScrollPane scrollPane = new JScrollPane();
@@ -125,40 +158,39 @@ public class MantenanceParamsForm extends FrameModel {
 		scrollPane.setViewportView(paramsPanel);
 		paramsPanel.setLayout(new BoxLayout(paramsPanel, BoxLayout.Y_AXIS));
 		
-		paramsPanel.add(new Parameter());
-		paramsPanel.setPreferredSize(new Dimension(10, (getPreferredSize().height + 50)));
-		paramsPanel.add(new Parameter());
-		paramsPanel.setPreferredSize(new Dimension(10, (getPreferredSize().height + 50)));
-		paramsPanel.add(new Parameter());
-		paramsPanel.setPreferredSize(new Dimension(10, (getPreferredSize().height + 50)));
-		paramsPanel.add(new Parameter());
-		paramsPanel.setPreferredSize(new Dimension(10, (getPreferredSize().height + 50)));
-		paramsPanel.add(new Parameter());
-		paramsPanel.setPreferredSize(new Dimension(10, (getPreferredSize().height + 50)));
-		paramsPanel.add(new Parameter());
-		paramsPanel.setPreferredSize(new Dimension(10, (getPreferredSize().height + 50)));
-		paramsPanel.add(new Parameter());
-		paramsPanel.setPreferredSize(new Dimension(10, (getPreferredSize().height + 50)));
-		paramsPanel.add(new Parameter());
-		paramsPanel.setPreferredSize(new Dimension(10, (getPreferredSize().height + 50)));
-		paramsPanel.add(new Parameter());
-		paramsPanel.setPreferredSize(new Dimension(10, (getPreferredSize().height + 50)));
-		paramsPanel.add(new Parameter());
-		paramsPanel.setPreferredSize(new Dimension(10, (getPreferredSize().height + 50)));
-		paramsPanel.add(new Parameter());
-		paramsPanel.setPreferredSize(new Dimension(10, (getPreferredSize().height + 50)));
-		paramsPanel.add(new Parameter());
-		paramsPanel.setPreferredSize(new Dimension(10, (getPreferredSize().height + 50)));
-		paramsPanel.add(new Parameter());
-		paramsPanel.setPreferredSize(new Dimension(10, (getPreferredSize().height + 50)));
-		paramsPanel.add(new Parameter());
-		paramsPanel.setPreferredSize(new Dimension(10, (getPreferredSize().height + 50)));
-		paramsPanel.add(new Parameter());
-		paramsPanel.setPreferredSize(new Dimension(10, (getPreferredSize().height + 50)));
-		paramsPanel.add(new Parameter());
-		paramsPanel.setPreferredSize(new Dimension(10, (getPreferredSize().height + 50)));
+		//llena los parametros
+		try {
+			list = getParameterDescriptionList();
+			paramsPanel.setPreferredSize(new Dimension(10, (list.size() * 50)));
+			
+			for(Parameter parameter : list) {
+				paramsPanel.add(parameter);
+			}
+			
+			
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+	
 	}
 
 	
 
+	private ArrayList<Parameter> getParameterDescriptionList() throws ClassNotFoundException, SQLException{
+		ArrayList<Parameter> list = new ArrayList<>();
+		
+		ParamMesuresDescriptions parametersDB = new ParamMesuresDescriptions();
+		ResultSet parameters = parametersDB.findRecords();
+		
+		while(parameters.next()) {
+			String id = parameters.getString("id");
+			String description = parameters.getString("description");
+			list.add(new Parameter(id, description));
+		}
+		
+		return list;
+		
+	}
+	
+	
 }
--- src/manteneceParamsForm/Parameter.java
+++ src/manteneceParamsForm/Parameter.java
@@ -2,76 +2,222 @@ package manteneceParamsForm;
 
 import javax.swing.JPanel;
 import java.awt.Dimension;
+import java.util.HashMap;
+
 import net.miginfocom.swing.MigLayout;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.border.MatteBorder;
+import components.Constants;
 import java.awt.Color;
 import javax.swing.SwingConstants;
+import java.awt.BorderLayout;
+import java.awt.Component;
+import javax.swing.Box;
 
 public class Parameter extends JPanel {
 
 	private static final long serialVersionUID = 1L;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
-	private JTextField textField_6;
+	private JLabel lblParameter;
+	private JTextField txtData1;
+	private JTextField txtData2;
+	private JTextField txtData3;
+	private JTextField txtData4;
+	private JTextField txtData5;
+	private JTextField txtData6;
+	private JTextField txtData7;
+	private String id = null;
 
-	public Parameter() {
+	public Parameter(String id, String description) {
+		this.id = id;
+		setBackground(Constants.getSurfaceColor());
+		setBorder(new MatteBorder(0, 1, 1, 0, Constants.getTextColor()));
 		setPreferredSize(new Dimension(1075, 50));
 		setMinimumSize(new Dimension(500, 50));
 		setMaximumSize(new Dimension(32767, 50));
 		setLayout(new MigLayout("insets 0, gap 0", "[300px:n,grow,fill][100px:100px:100px,grow][100px:100px:100px,grow][100px:100px:100px,grow][100px:100px:100px,grow][100px:100px:100px,grow][100px:100px:100px,grow][100px:100px:100px,grow]", "[grow,fill]"));
 		
-		JLabel lblParameter = new JLabel("Temperatura en la entrada de aire en la turbina");
-		lblParameter.setBorder(new MatteBorder(0, 1, 1, 1, (Color) Color.DARK_GRAY));
+		Component horizontalStrut_14 = Box.createHorizontalStrut(5);
+		add(horizontalStrut_14, "flowx,cell 0 0");
+		
+		lblParameter = new JLabel(description);
+		lblParameter.setForeground(Constants.getTextColor());
+		lblParameter.setBorder(new MatteBorder(0, 0, 0, 1, Constants.getTextColor()));
 		add(lblParameter, "cell 0 0,alignx leading,aligny center");
 		
-		textField = new JTextField();
-		textField.setHorizontalAlignment(SwingConstants.CENTER);
-		textField.setBorder(new MatteBorder(0, 0, 1, 1, (Color) Color.DARK_GRAY));
-		add(textField, "cell 1 0,grow");
-		textField.setColumns(10);
+		JPanel panel = new JPanel();
+		panel.setOpaque(false);
+		panel.setBorder(new MatteBorder(0, 0, 0, 1, Constants.getTextColor()));
+		add(panel, "cell 1 0,grow");
+		panel.setLayout(new BorderLayout());
 		
-		textField_1 = new JTextField();
-		textField_1.setHorizontalAlignment(SwingConstants.CENTER);
-		textField_1.setBorder(new MatteBorder(0, 0, 1, 1, (Color) Color.DARK_GRAY));
-		add(textField_1, "cell 2 0,grow");
-		textField_1.setColumns(10);
+		txtData1 = new JTextField();
+		txtData1.setHorizontalAlignment(JTextField.CENTER);
 		
-		textField_2 = new JTextField();
-		textField_2.setHorizontalAlignment(SwingConstants.CENTER);
-		textField_2.setBorder(new MatteBorder(0, 0, 1, 1, (Color) Color.DARK_GRAY));
-		add(textField_2, "cell 3 0,grow");
-		textField_2.setColumns(10);
+		panel.add(txtData1, BorderLayout.CENTER);
 		
-		textField_3 = new JTextField();
-		textField_3.setHorizontalAlignment(SwingConstants.CENTER);
-		textField_3.setBorder(new MatteBorder(0, 0, 1, 1, (Color) Color.DARK_GRAY));
-		add(textField_3, "cell 4 0,grow");
-		textField_3.setColumns(10);
+		Component horizontalStrut = Box.createHorizontalStrut(5);
+		panel.add(horizontalStrut, BorderLayout.EAST);
 		
-		textField_4 = new JTextField();
-		textField_4.setHorizontalAlignment(SwingConstants.CENTER);
-		textField_4.setBorder(new MatteBorder(0, 0, 1, 1, (Color) Color.DARK_GRAY));
-		add(textField_4, "cell 5 0,grow");
-		textField_4.setColumns(10);
+		Component horizontalStrut_1 = Box.createHorizontalStrut(5);
+		panel.add(horizontalStrut_1, BorderLayout.WEST);
 		
-		textField_5 = new JTextField();
-		textField_5.setHorizontalAlignment(SwingConstants.CENTER);
-		textField_5.setBorder(new MatteBorder(0, 0, 1, 1, (Color) Color.DARK_GRAY));
-		add(textField_5, "cell 6 0,grow");
-		textField_5.setColumns(10);
+		Component verticalStrut = Box.createVerticalStrut(5);
+		panel.add(verticalStrut, BorderLayout.SOUTH);
 		
-		textField_6 = new JTextField();
-		textField_6.setHorizontalAlignment(SwingConstants.CENTER);
-		textField_6.setBorder(new MatteBorder(0, 0, 1, 1, (Color) Color.DARK_GRAY));
-		add(textField_6, "cell 7 0,grow");
-		textField_6.setColumns(10);
-
+		Component verticalStrut_1 = Box.createVerticalStrut(5);
+		panel.add(verticalStrut_1, BorderLayout.NORTH);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setOpaque(false);
+		panel_1.setBorder(new MatteBorder(0, 0, 0, 1, Constants.getTextColor()));
+		add(panel_1, "cell 2 0,grow");
+		panel_1.setLayout(new BorderLayout());
+		
+		txtData2 = new JTextField();
+		txtData2.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_1.add(txtData2, BorderLayout.CENTER);
+		
+		Component horizontalStrut_2 = Box.createHorizontalStrut(5);
+		panel_1.add(horizontalStrut_2, BorderLayout.WEST);
+		
+		Component horizontalStrut_3 = Box.createHorizontalStrut(5);
+		panel_1.add(horizontalStrut_3, BorderLayout.EAST);
+		
+		Component verticalStrut_2 = Box.createVerticalStrut(5);
+		panel_1.add(verticalStrut_2, BorderLayout.NORTH);
+		
+		Component verticalStrut_3 = Box.createVerticalStrut(5);
+		panel_1.add(verticalStrut_3, BorderLayout.SOUTH);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setOpaque(false);
+		panel_2.setBorder(new MatteBorder(0, 0, 0, 1, Constants.getTextColor()));
+		add(panel_2, "cell 3 0,grow");
+		panel_2.setLayout(new BorderLayout());
+		
+		txtData3 = new JTextField();
+		txtData3.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_2.add(txtData3, BorderLayout.CENTER);
+		
+		Component horizontalStrut_4 = Box.createHorizontalStrut(5);
+		panel_2.add(horizontalStrut_4, BorderLayout.WEST);
+		
+		Component horizontalStrut_5 = Box.createHorizontalStrut(5);
+		panel_2.add(horizontalStrut_5, BorderLayout.EAST);
+		
+		Component verticalStrut_4 = Box.createVerticalStrut(5);
+		panel_2.add(verticalStrut_4, BorderLayout.NORTH);
+		
+		Component verticalStrut_5 = Box.createVerticalStrut(5);
+		panel_2.add(verticalStrut_5, BorderLayout.SOUTH);
+		
+		JPanel panel_4 = new JPanel();
+		panel_4.setOpaque(false);
+		panel_4.setBorder(new MatteBorder(0, 0, 0, 1, Constants.getTextColor()));
+		add(panel_4, "cell 4 0,grow");
+		panel_4.setLayout(new BorderLayout());
+		
+		txtData4 = new JTextField();
+		txtData4.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_4.add(txtData4, BorderLayout.CENTER);
+		
+		Component horizontalStrut_6 = Box.createHorizontalStrut(5);
+		panel_4.add(horizontalStrut_6, BorderLayout.WEST);
+		
+		Component horizontalStrut_7 = Box.createHorizontalStrut(5);
+		panel_4.add(horizontalStrut_7, BorderLayout.EAST);
+		
+		Component verticalStrut_6 = Box.createVerticalStrut(5);
+		panel_4.add(verticalStrut_6, BorderLayout.NORTH);
+		
+		Component verticalStrut_7 = Box.createVerticalStrut(5);
+		panel_4.add(verticalStrut_7, BorderLayout.SOUTH);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setOpaque(false);
+		panel_3.setBorder(new MatteBorder(0, 0, 0, 1, Constants.getTextColor()));
+		add(panel_3, "cell 5 0,grow");
+		panel_3.setLayout(new BorderLayout());
+		
+		txtData5 = new JTextField();
+		txtData5.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_3.add(txtData5, BorderLayout.CENTER);
+		
+		Component horizontalStrut_8 = Box.createHorizontalStrut(5);
+		panel_3.add(horizontalStrut_8, BorderLayout.WEST);
+		
+		Component horizontalStrut_9 = Box.createHorizontalStrut(5);
+		panel_3.add(horizontalStrut_9, BorderLayout.EAST);
+		
+		Component verticalStrut_8 = Box.createVerticalStrut(5);
+		panel_3.add(verticalStrut_8, BorderLayout.NORTH);
+		
+		Component verticalStrut_9 = Box.createVerticalStrut(5);
+		panel_3.add(verticalStrut_9, BorderLayout.SOUTH);
+		
+		JPanel panel_5 = new JPanel();
+		panel_5.setOpaque(false);
+		panel_5.setBorder(new MatteBorder(0, 0, 0, 1, Constants.getTextColor()));
+		add(panel_5, "cell 6 0,grow");
+		panel_5.setLayout(new BorderLayout());
+		
+		txtData6 = new JTextField();
+		txtData6.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_5.add(txtData6, BorderLayout.CENTER);
+		
+		Component horizontalStrut_10 = Box.createHorizontalStrut(5);
+		panel_5.add(horizontalStrut_10, BorderLayout.WEST);
+		
+		Component horizontalStrut_11 = Box.createHorizontalStrut(5);
+		panel_5.add(horizontalStrut_11, BorderLayout.EAST);
+		
+		Component verticalStrut_10 = Box.createVerticalStrut(5);
+		panel_5.add(verticalStrut_10, BorderLayout.NORTH);
+		
+		Component verticalStrut_11 = Box.createVerticalStrut(5);
+		panel_5.add(verticalStrut_11, BorderLayout.SOUTH);
+		
+		JPanel panel_6 = new JPanel();
+		panel_6.setOpaque(false);
+		panel_6.setBorder(new MatteBorder(0, 0, 0, 1, Constants.getTextColor()));
+		add(panel_6, "cell 7 0,grow");
+		panel_6.setLayout(new BorderLayout());
+		
+		txtData7 = new JTextField();
+		txtData7.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_6.add(txtData7, BorderLayout.CENTER);
+		
+		Component horizontalStrut_12 = Box.createHorizontalStrut(5);
+		panel_6.add(horizontalStrut_12, BorderLayout.WEST);
+		
+		Component horizontalStrut_13 = Box.createHorizontalStrut(5);
+		panel_6.add(horizontalStrut_13, BorderLayout.EAST);
+		
+		Component verticalStrut_12 = Box.createVerticalStrut(5);
+		panel_6.add(verticalStrut_12, BorderLayout.NORTH);
+		
+		Component verticalStrut_13 = Box.createVerticalStrut(5);
+		panel_6.add(verticalStrut_13, BorderLayout.SOUTH);
+		
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public HashMap<String, String> getData(){
+		HashMap<String, String> data = new HashMap<>();
+		data.put("data1", txtData1.getText());
+		data.put("data2", txtData2.getText());
+		data.put("data3", txtData3.getText());
+		data.put("data4", txtData4.getText());
+		data.put("data5", txtData5.getText());
+		data.put("data6", txtData6.getText());
+		data.put("data7", txtData7.getText());
+		
+		return data;
+		
 	}
 
 }
